6df983d9bfa3f177c8a8dc9691b02b15a756c71b
jsm-txt <hose153@gmail.com>
2021-01-11 16:37:27 -0600

    Added create image and pet template
    Templates to added pets add images to pets and url links to work.

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/petDatabase"
+mongo = PyMongo(app)
+
 ############################################################
 # ROUTES
 ############################################################
@@ -16,12 +19,55 @@ app = Flask(__name__)
 @app.route('/')
 def pet_list():
     
-    pet_list = ['poodle','cow']
+    pet_data = mongo.db.pet.find()
+
     context = {
-        'pet_list' : pet_list,
+        'pet_list' : pet_data
     }
-    
+
     return render_template('pet_list.html', **context)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        new_pet = {
+            'name': request.form.get("pet_name"),
+            'photo_url': request.form.get("photo"),
+        }
+        result = mongo.db.pet.insert_one(new_pet)
+        pet_id = result.inserted_id
+
+        return redirect(url_for('pet', pet_id= pet_id))
+    else:
+        return render_template('create.html')
+
+
+@app.route('/pet/<pet_id>')
+def pet(pet_id):
+    
+    pet_display = mongo.db.pet.find_one({'_id': ObjectId(pet_id)})
+    pet_images = mongo.db.pet_pictures.find({'pet_id': pet_id})
+    context = {
+        'pet' : pet_display,
+        'pet_pictures' : pet_images
+    }
+    return render_template('pet.html', **context)
+
+
+@app.route('/images/<pet_id>', methods=['GET', 'POST'])
+def images(pet_id):
+    if request.method == 'POST':
+        pet_picture = {
+            'pet_pictures': request.form.get("photo"),
+            'pet_id': pet_id
+        }
+        mongo.db.pet_pictures.insert_one(pet_picture)
+        
+
+        return redirect(url_for('pet', pet_id= pet_id))
+    else:
+        return render_template('images.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
