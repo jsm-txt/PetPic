83e6be13353ee9db18adfa2f6288481e04f3bde4
jsm-txt <hose153@gmail.com>
2021-01-10 20:39:57 -0600

    Template and routes started
    Template is getting set up, hard coded list made with a for loop

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 ############################################################
@@ -7,16 +9,19 @@ from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017"
-# mongo = PyMongo(app)
-
 ############################################################
 # ROUTES
 ############################################################
 
 @app.route('/')
 def pet_list():
-    return render_template('base.html')
+    
+    pet_list = ['poodle','cow']
+    context = {
+        'pet_list' : pet_list,
+    }
+    
+    return render_template('pet_list.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
